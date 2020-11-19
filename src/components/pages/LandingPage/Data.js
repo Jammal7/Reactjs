82400858163a12fa97b8cce695ea8aa8220c0a05
Hardeyemi7 <Hardeyemi7@users.noreply.github.com>
2020-11-19 13:22:28 +0100

    First Commit

---
+++ src/components/pages/LandingPage/Data.js
@@ -0,0 +1,11 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    headLine: 'Hums Medical we care about your health',
+    buttonLabel: 'SIGN UP',
+    buttonLabel1: 'SIGN IN',
+    imgStart: '',
+    img: 'image/Medicine-amico.svg',
+    alt: 'HumsMedical'
+}
